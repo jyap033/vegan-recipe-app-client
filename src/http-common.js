b2257e9c23ccedac83e152c90f3bab2a526e14c5
jyap033 <49565266+jyap033@users.noreply.github.com>
2021-03-21 18:16:16 +0800

    commit 1

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,12 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://vegan-recipe-app.herokuapp.com/api",
-  
+
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    withCredentials: false,
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
   }
 });
